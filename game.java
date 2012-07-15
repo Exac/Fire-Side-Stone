4fdd8b75d8a995e7151536d45b3dc3b5591a3ced
unknown <User@User-PC.(none)>
2012-07-15 01:34:21 -0500

    added up/down scrolling

--- game.java
+++ game.java
@@ -5,6 +5,7 @@ import java.awt.*;
 public class game extends Applet implements KeyListener
 {
 	int i = 0;
+	int j = 0;
  	AudioClip soundFile1;
 	AudioClip soundFile2;
 	Image snoopy;
@@ -14,33 +15,43 @@ public class game extends Applet implements KeyListener
 		soundFile1 = getAudioClip(getDocumentBase(),"BEAST_FICTION.wav");
 		soundFile2 = getAudioClip(getDocumentBase(),"shootS.au");
 		snoopy = getImage(getDocumentBase(),"snoopy.gif");
-		background = getImage(getDocumentBase(),"back.jpg");
+		background = getImage(getDocumentBase(),"testback.jpg");
 		addKeyListener(this);
 		soundFile1.play();
 	}
+
 	public void paint(Graphics g)
 	{
-		g.drawImage(background,0-i,0,this);
-		g.drawImage(background,697-i,0,this);
+		g.drawImage(background,0-i,j,this);
+		g.drawImage(background,700-i,j,this);
+
 		g.drawImage(snoopy,300,180,this);
-		i+=2;
-		if(i>697)
-			i-=697;
+
+		i+=2; // Gives continuous scrolling while keys aren't doing shit
+
+		//Restarts the background
+		if(i>700)
+			i-=700;
 		if(i<0)
-			i+=697;
+			i+=700;
 	}
+
 	public void keyPressed(KeyEvent ke) {
 		switch(ke.getKeyCode())
 		{
 			case KeyEvent.VK_DOWN:
+			//	if (j != -70) {
+					j-=5;// }
 				break;
 			case KeyEvent.VK_RIGHT:
-				i+=5;
+				i+=3;
 				break;
 			case KeyEvent.VK_LEFT:
-				i-=5;
+				i-=3;
 				break;
 			case KeyEvent.VK_UP:
+				if (j < 0) { //Limitations so the view does not fuck over
+					j+=5;	}
 				break;
 		}
 	}
