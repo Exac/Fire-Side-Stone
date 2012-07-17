361620a9e9550fafc53e519a94e74555cfaa624e
Jonathan Jao <jonathan@jonathan-NY591AAR-ABA-p6267c.(none)>
2012-07-17 18:23:21 -0500

    oops, this the real previous commit

--- game.java
+++ game.java
@@ -1,13 +1,17 @@
 import java.applet.*;
 import java.awt.event.*;
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+
 public class game extends Applet implements KeyListener
 {
 	int screenSizeX = 700, screenSizeY=350;
 	int cx = 300, cy=180;
-	int camera=1;
+	int camera=2;
 	int objects_index=0;
+	int buffer;
  	AudioClip soundFile1;
 	ArrayList<oBase> objects = new ArrayList<oBase>();
 	public void init()
@@ -15,14 +19,45 @@ public class game extends Applet implements KeyListener
 		soundFile1 = getAudioClip(getDocumentBase(),"music/01.wav");
 		addKeyListener(this);
 		soundFile1.play();
+		try{
+			Scanner sc = new Scanner(new File("objects.dat"));
+			camera=sc.nextInt();
+			objects_index=sc.nextInt();
+			/*
+			for(int i = 0; i < objects_index; i++)
+			{
+				buffer=sc.nextInt();
+				if(buffer==0)
+				{
+					objects.add(new oBase(sc.nextLine(),sc.nextInt(),sc.nextInt(),this));
+				}
+				else if(buffer==1)
+				{
+					objects.add(new oChar(sc.nextLine(),sc.nextInt(),sc.nextInt(),this));
+				}
+				else if(buffer==2)
+				{
+					objects.add(new oBack(sc.nextLine(),sc.nextInt(),sc.nextInt(),this));
+				}
+
+			}
+			*/
+
+
+	        } catch (FileNotFoundException e) {
+	            soundFile1 = getAudioClip(getDocumentBase(),"music/02.wav");
+		addKeyListener(this);
+		soundFile1.play();
+	        }
 		objects.add(new oBack("backgrounds/01.jpg",0,0,this));
 		objects_index++;
 		objects.add(new oChar("img/snoopy.gif",380,100,this));
 		objects.get(1).hspeed = 5;
 		objects_index++;
-		objects.add(new oChar("img/snoopy.gif",300,100,this));
+		objects.add(new oChar("img/barrel.gif",300,100,this));
 		objects.get(2).hspeed = 5;
 		objects_index++;
+
 	}
 
 	public void paint(Graphics g)
